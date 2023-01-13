60abeec384751720fddd0a943c6d11c736632d8a
Shaira <sil21019@byui.edu>
2023-01-13 11:59:52 +0000

    lesson04

--- contacts-app/routes/index.js
+++ contacts-app/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const home = require('../controllers');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 router.use('/contacts', require('./contacts'))
 router.use('/', home.displayName)
 
---
+++ contacts-app/swagger.js
@@ -0,0 +1,21 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Contacts API'
+  },
+  host: 'shaira341-w01-personal-assignment.onrender.com',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./server.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
