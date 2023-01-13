22d7cf5e7f22a7b881314342cc7fc91870030cb2
Shaira <sil21019@byui.edu>
2023-01-13 19:22:28 +0000

    disabled setHeader - returned normal swagger ui render

--- contacts-app/server.js
+++ contacts-app/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongodb = require('./db/connect');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -8,16 +8,17 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app
-  .use(bodyParser.json())
+  .use(cors())
+  .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader(
-    //   'Access-Control-Allow-Headers',
-    //   'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    // );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
     // res.setHeader('Content-Type', 'application/json');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
