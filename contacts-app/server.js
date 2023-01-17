bc8cafedfc619b0f5611c958040ef32125427586
Shaira <sil21019@byui.edu>
2023-01-17 18:17:37 +0000

    placed routes above setHeader in server.js  - renders swagger ui ok

--- contacts-app/server.js
+++ contacts-app/server.js
@@ -11,17 +11,18 @@ app
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
+  .use('/', require('./routes'))
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
-    // res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
-  .use('/', require('./routes'));
+  
 
 mongodb.accessDb((err, mongodb) => {
   if (err) {
