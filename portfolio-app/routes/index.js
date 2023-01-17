3297c78d3055a854b93675976f793ae4794df61b
Shaira <sil21019@byui.edu>
2023-01-17 23:09:55 +0000

    portfolio app api documentation added

--- portfolio-app/routes/index.js
+++ portfolio-app/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const home = require('../controllers');
 
-// router.use('/', require('./swagger.js'));
+router.use('/', require('./swagger.js'));
 router.use('/projects', require('./projects'))
 router.use('/', home.displayHome)
 
