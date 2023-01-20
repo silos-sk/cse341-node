04dfb2d6403fa3a7f1b0714c5704f42b600fdc32
Shaira <sil21019@byui.edu>
2023-01-20 21:20:58 +0000

    joi validation added to swagger

--- portfolio-app/swagger.js
+++ portfolio-app/swagger.js
@@ -1,10 +1,16 @@
 const swaggerAutogen = require('swagger-autogen')();
+const j2s = require('joi-to-swagger');
+const projSchema = require('./helpers/validation_schema')
+
+const { swagger } = j2s(projSchema);
 
 const doc = {
   info: {
     title: 'Projects and Courses API',
     description: 'This is an API server for my personal portfolio website'
   },
+  '@definitions': {
+    PayloadA: swagger},
   host: 'portfolio-app-iu9g.onrender.com',
   schemes: ['https']
 };
@@ -14,6 +20,7 @@ const endpointsFiles = ['./server.js'];
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
+
 
 // Run server after it gets generated
 // swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
