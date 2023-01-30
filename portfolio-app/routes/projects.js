63c556b1d73a814daacb9783beecae90a6bd9464
Shaira <sil21019@byui.edu>
2023-01-30 16:58:22 +0000

    post auth set up

--- portfolio-app/routes/projects.js
+++ portfolio-app/routes/projects.js
@@ -4,8 +4,17 @@ const projects = require('../controllers/project');
 // const checkAuth = require('../helpers/auth')
 const auth = require('../middleware/authenticateToken')
 
+// Auth0
+const { requiresAuth } = require('express-openid-connect');
+
 router.get('/', projects.getData);
-router.post('/', auth.authenticateToken, projects.createDoc) 
+
+// auth try
+// router.post('/', auth.authenticateToken, projects.createDoc) 
+
+// Auth0
+router.post('/', requiresAuth(), projects.createDoc) 
+
 router.route('/:id').get(projects.getDocById).put(projects.updateDoc).delete(projects.removeDoc)
 
 
